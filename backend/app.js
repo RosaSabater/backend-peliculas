92e243a85c4553390d9c511ca3e4cab5487a528a
Rosa <rosamari.sab@gmail.com>
2020-10-05 13:26:02 +0200

    created first model Peliculas

---
+++ backend/app.js
@@ -0,0 +1,29 @@
+const express = require('express');
+const app = express();
+
+const conn = mysql.createConnection({
+    host: 'localhost',
+    user: 'root',
+    database: 'dbPeliculas',
+})
+
+app.listen(3000,()=>console.log('Servidor levantado en 3000'));
+
+app.get('/',(req, res)=> res.send('Nitflix frontpage'));
+
+app.get('/login', (req, res) => res.send('Login')); //iniciar sesion
+
+app.get('/registro', (req, res) => res.send('Registro')); //registrar nueva cuenta
+
+app.get('/logout', (req, res) => res.send('Logout')); //cerrar sesión
+
+app.get('/perfil', (req, res) => res.send('Perfil')); //perfil de la cuenta con info y peliculas alquiladas
+
+//esto es para evitar el error de CORS
+app.use(function(req, res, next) {
+    res.header("Access-Control-Allow-Origin", "*"); //permite hacer peticiones desde todos los orígenes
+    res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept"); //permite peticiones con las cabeceras enumeradas
+    // res.header("Access-Control-Allow-Methods", "GET, POST");
+    next();
+});
+
